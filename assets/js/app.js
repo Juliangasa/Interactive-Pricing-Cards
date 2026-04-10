9de62b8a024d7c044bdbd43aec1e332b66139192
Julian <juliangasa7@outlook.com>
2026-04-10 10:15:08 -0500

    project resumed; currently developing a JS file

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+const prices = {
+  monthly: {
+    basic: 9,
+    pro: 19,
+    enterprise: 49,
+},
+yearly: {
+  basic: 90,
+  pro: 190,
+  enterprise: 490,
+},
+};
+let billingPeriod = "monthly";
